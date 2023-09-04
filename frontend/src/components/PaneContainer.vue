7ef52b7f6cf5263202dbfac23bd263a6aee325d3
csterritt <csterritt@gmail.com>
2023-09-04 15:38:44 -0400

    Set up arrow-up-down icon for toggle button.

--- frontend/src/components/PaneContainer.vue
+++ frontend/src/components/PaneContainer.vue
@@ -4,8 +4,15 @@
 
     <div class="flex flex-row">
       <div class="flex-grow divider">
-        <button class="btn btn-secondary" @click="store.togglePaneState">
-          T
+        <button
+          class="btn btn-square btn-accent"
+          @click="store.togglePaneState"
+        >
+          <img
+            alt="toggle panes"
+            class="w-8 h-8"
+            src="../assets/icons/arrow-up-341-svgrepo-com.svg"
+          />
         </button>
       </div>
     </div>
